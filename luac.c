fe573c7d8af7fa8cb0431a68afdfcf5c11e2222d
Luiz Henrique de Figueiredo <lhf@tecgraf.puc-rio.br>
1996-02-21 14:20:42 -0300

    arg processing dump file

--- luac.c
+++ luac.c
@@ -1,9 +1,9 @@
 /*
 ** luac.c
-** lua compiler (saves bytecodes in files)
+** lua compiler (saves bytecodes to files)
 */
 
-char *rcs_luac="$Id: luac.c,v 1.1 1996/02/20 16:39:40 lhf Exp lhf $";
+char *rcs_luac="$Id: luac.c,v 1.2 1996/02/20 21:58:39 lhf Exp lhf $";
 
 #include <stdio.h>
 #include <string.h>
@@ -11,69 +11,99 @@ char *rcs_luac="$Id: luac.c,v 1.1 1996/02/20 16:39:40 lhf Exp lhf $";
 
 #include "lua.h"
 #include "tree.h"
-#include "inout.h"
 #include "func.h"
+#include "inout.h"
+#include "mem.h"
 #include "opcode.h"
 
-void PrintCode(Byte *code, Byte *end);
+void PrintFunction(TFunc *tf);
+void DumpFunction(TFunc *tf, FILE *D);
+
 static void compile(char *filename);
 
-extern jmp_buf *errorJmp;
+static int listing=0;
+static FILE *D;
+
+#define	IS(s)	(strcmp(argv[i],s)==0)
+
+static void usage(void)
+{
+ fprintf(stderr,"usage: luac [-v] [-l] [-o filename] file ...\n");
+ exit(0);
+}
 
 int main(int argc, char *argv[])
 {
- if (argc < 2)
-  compile(NULL);  /* executes stdin as a file */
- else
+ char *d="luac.out";
+ int i;
+ for (i=1; i<argc; i++)
  {
-  int i;
-  for (i=1; i<argc; i++)
-  {
-   if (strcmp(argv[i], "-") == 0)
-    compile(NULL);  /* executes stdin as a file */
-   else if (strcmp(argv[i], "-v") == 0)
-    printf("%s  %s\n(written by %s)\n\n",
-            LUA_VERSION, LUA_COPYRIGHT, LUA_AUTHORS);
-   else
-    compile(argv[i]);
-  }
+  if (argv[i][0]!='-')			/* end of options */
+   break;
+  else if (IS("-"))			/* use stdin */
+   break;
+  else if (IS("-v"))			/* show version */
+   printf("%s  %s\n(written by %s)\n\n",LUA_VERSION,LUA_COPYRIGHT,LUA_AUTHORS);
+  else if (IS("-l"))			/* list */
+   listing=1;
+  else if (IS("-o"))			/* output file */
+   d=argv[++i];
+  else
+   usage();
  }
+ --i;					/* fake new argv[0] */
+ argc-=i;
+ argv+=i;
+ if (argc<2) usage();
+ D=fopen(d,"wb");			/* must be binary mode */
+ if (D==NULL)
+ {
+  fprintf(stderr,"luac: cannot open ");
+  perror(d);
+  exit(1);
+ }
+ for (i=1; i<argc; i++) compile(IS("-")? NULL : argv[i]);
+ fclose(D);
  return 0;
 }
 
-static void dump(TFunc *tf)
+void dump(TFunc *tf)
 {
-  printf("dump: %s:%d %d bytes\n",tf->fileName,tf->lineDefined,tf->size);
-  PrintCode(tf->code,tf->code+tf->size);
-  ThreadCode(tf->code,tf->code+tf->size);
-  PrintCode(tf->code,tf->code+tf->size);
+ if (listing) PrintFunction(tf);
+ DumpFunction(tf,D);
+ luaI_free(tf->code);
 }
 
 static void do_compile(void)
 {
-  TFunc tf;
-  jmp_buf myErrorJmp;
-  jmp_buf *oldErr = errorJmp;
-  errorJmp = &myErrorJmp;
-  luaI_initTFunc(&tf);
-  tf.fileName = lua_parsedfile;
-  if (setjmp(myErrorJmp) == 0)
-  {
-    lua_parse(&tf);
-    dump(&tf);
-  }
-  else				/* syntax error */
-  {
-    exit(1);
-  }
-  errorJmp = oldErr;
-  if (tf.code)
-    luaI_free(tf.code);
+ TFunc tf;
+ extern jmp_buf *errorJmp;
+ jmp_buf myErrorJmp;
+ jmp_buf *oldErr = errorJmp;
+ errorJmp = &myErrorJmp;
+ luaI_initTFunc(&tf);
+ tf.fileName = lua_parsedfile;
+ if (setjmp(myErrorJmp) == 0)
+ {
+  lua_parse(&tf);
+  tf.marked=0;
+  dump(&tf);
+ }
+ else					/* syntax error */
+ {
+  exit(1);
+ }
+ errorJmp = oldErr;
 }
 
 static void compile(char *filename)
 {
-  if (lua_openfile(filename)) return;
-  do_compile();
-  lua_closefile();
+ if (lua_openfile(filename))
+ {
+  fprintf(stderr,"luac: cannot open ");
+  perror(filename);
+  exit(1);
+ }
+ do_compile();
+ lua_closefile();
 }
