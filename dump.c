ac0ad8b6fe4d5d6052b2c19d238976710f6735bc
Luiz Henrique de Figueiredo <lhf@tecgraf.puc-rio.br>
1996-02-23 16:02:26 -0300

    moved print to a new module added macros to access fields in global tables added luac.h changed signature

--- dump.c
+++ dump.c
@@ -1,168 +1,15 @@
 /*
 ** dump.c
-** print,thread,and saves bytecodes to files
+** thread and save bytecodes to file
 */
 
-char *rcs_dump="$Id$";
+char *rcs_dump="$Id: dump.c,v 1.4 1996/02/22 15:33:13 lhf Exp lhf $";
 
 #include <stdio.h>
 #include <string.h>
-#include "opcode.h"
-#include "table.h"
-#include "tree.h"
-#include "types.h"
-#include "dump.h"
+#include "luac.h"
 
-#define VarStr(i)	(lua_table[i].varname->str)
-#define VarLoc(i)	(lua_table[i].varname->varindex)
-#define StrStr(i)	(lua_constant[i]->str)
-#define StrLoc(i)	(lua_constant[i]->constindex)
-
-extern Word lua_ntable;
-extern Word lua_nconstant;
-static TFunc *lastF=NULL;
-
-static void PrintCode(Byte *code, Byte *end)
-{
- Byte *p;
- for (p=code; p!=end;)
- {
-	OpCode op=(OpCode)*p;
-	if (op>SETLINE) op=SETLINE+1;
-	printf("%8d   %s",p-code,OpCodeName[op]);
-	switch (op)
-	{
-	case PUSHNIL:
-	case PUSH0:
-	case PUSH1:
-	case PUSH2:
-	case PUSHLOCAL0:
-	case PUSHLOCAL1:
-	case PUSHLOCAL2:
-	case PUSHLOCAL3:
-	case PUSHLOCAL4:
-	case PUSHLOCAL5:
-	case PUSHLOCAL6:
-	case PUSHLOCAL7:
-	case PUSHLOCAL8:
-	case PUSHLOCAL9:
-	case PUSHINDEXED:
-	case STORELOCAL0:
-	case STORELOCAL1:
-	case STORELOCAL2:
-	case STORELOCAL3:
-	case STORELOCAL4:
-	case STORELOCAL5:
-	case STORELOCAL6:
-	case STORELOCAL7:
-	case STORELOCAL8:
-	case STORELOCAL9:
-	case STOREINDEXED0:
-	case ADJUST0:
-	case EQOP:
-	case LTOP:
-	case LEOP:
-	case GTOP:
-	case GEOP:
-	case ADDOP:
-	case SUBOP:
-	case MULTOP:
-	case DIVOP:
-	case POWOP:
-	case CONCOP:
-	case MINUSOP:
-	case NOTOP:
-	case POP:
-	case RETCODE0:
-		p++;
-		break;
-	case PUSHBYTE:
-	case PUSHLOCAL:
-	case STORELOCAL:
-	case STOREINDEXED:
-	case STORELIST0:
-	case ADJUST:
-	case RETCODE:
-		printf("\t%d",*(p+1));
-		p+=2;
-		break;
-	case PUSHWORD:
-	case PUSHSELF:
-	case CREATEARRAY:
-	case ONTJMP:
-	case ONFJMP:
-	case JMP:
-	case UPJMP:
-	case IFFJMP:
-	case IFFUPJMP:
-	case SETLINE:
-	{
-		CodeWord c;
-		p++;
-		get_word(c,p);
-		printf("\t%d",c.w);
-		break;
-	}
-	case PUSHFLOAT:
-	{
-		CodeFloat c;
-		p++;
-		get_float(c,p);
-		printf("\t%g",c.f);
-		break;
-	}
-	case PUSHSTRING:
-	{
-		CodeWord c;
-		p++;
-		get_word(c,p);
-		printf("\t%d\t; \"%s\"",c.w,StrStr(c.w));
-		break;
-	}
-	case PUSHFUNCTION:
-	{
-		CodeCode c;
-		p++;
-		get_code(c,p);
-		printf("\t%p",c.tf);
-		break;
-	}
-	case PUSHGLOBAL:
-	case STOREGLOBAL:
-	{
-		CodeWord c;
-		p++;
-		get_word(c,p);
-		printf("\t%d\t; %s",c.w,VarStr(c.w));
-		break;
-	}
-	case STORELIST:
-	case CALLFUNC:
-		printf("\t%d %d",*(p+1),*(p+2));
-		p+=3;
-		break;
-	case STORERECORD:
-	{
-		int n=*++p;
-		printf("\t%d",n);
-		p++;
-		while (n--)
-		{
-			CodeWord c;
-			printf("\n%8d\tFIELD",p-code);
-			get_word(c,p);
-			printf("\t%d\t; \"%s\"",c.w,StrStr(c.w));
-		}
-		break;
-	}
-	default:
-		printf("\tcannot happen:  opcode=%d",*p);
-		p++;
-		break;
-	}
-	printf("\n");
- }
-}
+static TFunc *lastF=NULL;		/* list of functions seen in code */
 
 static int SawVar(int i, int at)
 {
@@ -317,11 +164,10 @@ static void ThreadCode(Byte *code, Byte *end)
 
 static void CheckThread(Byte *p, int i)
 {
- while (i>0)
+ while (i!=0)
  {
   CodeWord c;
-  Byte *q;
-  q=p+i;
+  Byte *q=p+i;
   printf(" %d",i);
   get_word(c,q);
   i=c.w;
@@ -356,15 +202,13 @@ static void DumpWord(int i, FILE *D)
 
 static void DumpString(char *s, FILE *D)
 {
- Word w;
  int n=strlen(s)+1;
  if (n>0xFFFF)
  {
   fprintf(stderr,"luac: string too long: \"%.32s...\"\n",s);
   exit(1);
  }
- w=n;
- fwrite(&w,sizeof(w),1,D);
+ DumpWord(n,D);
  fwrite(s,n,1,D);
 }
 
@@ -403,19 +247,11 @@ CheckThreads(tf->code);
 
 void DumpHeader(FILE *D)
 {
- Word w=0x1234;
- float f=1.234567890e-23;
- fputs("\033Lua\002\003",D);		/* <ESC> "Lua" version */
+ Word w=TEST_WORD;
+ float f=TEST_FLOAT;
+ fputc(ESC,D);
+ DumpString(SIGNATURE,D);		/* signature */
+ fputc(VERSION,D);			/* version */
  fwrite(&w,sizeof(w),1,D);		/* a word for testing byte ordering */
  fwrite(&f,sizeof(f),1,D);		/* a float for testing byte ordering */
-}
-
-void PrintFunction(TFunc *tf)
-{
- if (tf->lineDefined==0)
-  printf("\nmain of \"%s\" (%d bytes)\n",tf->fileName,tf->size);
- else
-  printf("\nfunction \"%s\":%d (%d bytes); used at main+%d\n",
-	tf->fileName,tf->lineDefined,tf->size,tf->marked);
- PrintCode(tf->code,tf->code+tf->size);
 }
