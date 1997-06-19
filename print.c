f146d3b5460ae4f4573968b456c8d993500dc41c
Luiz Henrique de Figueiredo <lhf@tecgraf.puc-rio.br>
1997-06-19 11:56:04 -0300

    new function LinkFunctions added heuristics to print name of functions

--- print.c
+++ print.c
@@ -3,13 +3,125 @@
 ** print bytecodes
 */
 
-char* rcs_print="$Id: print.c,v 1.12 1997/04/14 12:12:40 lhf Exp lhf $";
+char* rcs_print="$Id: print.c,v 1.13 1997/05/13 12:31:58 lhf Exp lhf $";
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include "luac.h"
 #include "print.h"
+
+void LinkFunctions(TFunc* m)
+{
+ static TFunc* lastF;			/* list of functions seen in code */
+ Byte* code=m->code;
+ Byte* end=code+m->size;
+ Byte* p;
+ if (IsMain(m)) lastF=m;
+ for (p=code; p!=end;)
+ {
+	int op=*p;
+	int at=p-code+1;
+	switch (op)
+	{
+	case PUSHNIL:
+	case PUSH0:
+	case PUSH1:
+	case PUSH2:
+	case PUSHLOCAL0:
+	case PUSHLOCAL1:
+	case PUSHLOCAL2:
+	case PUSHLOCAL3:
+	case PUSHLOCAL4:
+	case PUSHLOCAL5:
+	case PUSHLOCAL6:
+	case PUSHLOCAL7:
+	case PUSHLOCAL8:
+	case PUSHLOCAL9:
+	case PUSHINDEXED:
+	case STORELOCAL0:
+	case STORELOCAL1:
+	case STORELOCAL2:
+	case STORELOCAL3:
+	case STORELOCAL4:
+	case STORELOCAL5:
+	case STORELOCAL6:
+	case STORELOCAL7:
+	case STORELOCAL8:
+	case STORELOCAL9:
+	case STOREINDEXED0:
+	case ADJUST0:
+	case EQOP:
+	case LTOP:
+	case LEOP:
+	case GTOP:
+	case GEOP:
+	case ADDOP:
+	case SUBOP:
+	case MULTOP:
+	case DIVOP:
+	case POWOP:
+	case CONCOP:
+	case MINUSOP:
+	case NOTOP:
+	case POP:
+	case RETCODE0:
+		p++;
+		break;
+	case PUSHBYTE:
+	case PUSHLOCAL:
+	case STORELOCAL:
+	case STOREINDEXED:
+	case STORELIST0:
+	case ADJUST:
+	case RETCODE:
+	case VARARGS:
+	case STOREMAP:
+		p+=2;
+		break;
+	case PUSHWORD:
+	case PUSHSTRING:
+	case PUSHGLOBAL:
+	case PUSHSELF:
+	case STOREGLOBAL:
+	case CREATEARRAY:
+	case ONTJMP:
+	case ONFJMP:
+	case JMP:
+	case UPJMP:
+	case IFFJMP:
+	case IFFUPJMP:
+	case CALLFUNC:
+	case SETLINE:
+	case STORELIST:
+		p+=3;
+		break;
+	case PUSHFLOAT:
+		p+=5;			/* assumes sizeof(float)==4 */
+		break;
+	case PUSHFUNCTION:
+	{
+		TFunc* tf;
+		p++;
+		get_code(tf,p);
+		tf->marked=at;
+		tf->next=NULL;		/* TODO: remove? */
+		lastF=lastF->next=tf;
+		break;
+	}
+	case STORERECORD:
+	{
+		int n=*++p;
+		p+=2*n+1;
+		break;
+	}
+	default:			/* cannot happen */
+		fprintf(stderr,"luac: bad opcode %d at %d\n",*p,(int)(p-code));
+		exit(1);
+		break;
+	}
+ }
+}
 
 static LocVar* V=NULL;
 
@@ -204,8 +316,35 @@ void PrintFunction(TFunc* tf)
  if (IsMain(tf))
   printf("\nmain of \"%s\" (%d bytes at %p)\n",tf->fileName,tf->size,tf);
  else
-  printf("\nfunction \"%s\":%d (%d bytes at %p); used at main+%d\n",
+ {
+  extern TFunc* MAIN;
+  Byte* p=MAIN->code+tf->marked+sizeof(TFunc*);
+  printf("\nfunction ");
+  switch (*p)
+  {
+   case STOREGLOBAL:
+   {
+    Word w;
+    p++; get_word(w,p); printf("%s defined at ",VarStr(w));
+    break;
+   }
+   case STOREINDEXED0:			/* try method definition */
+   {
+    if (p[-11]==PUSHGLOBAL && p[-8]==PUSHSTRING)
+    {
+     Byte* pp=p;
+     Word w;
+     p-=11; p++; get_word(w,p); printf("%s:",VarStr(w));
+     p=pp;
+     p-=8;  p++; get_word(w,p); printf("%s defined at ",StrStr(w));
+     p=pp;
+    }
+    break;
+   }
+  }
+  printf("\"%s\":%d (%d bytes at %p); used at main+%d\n",
 	tf->fileName,tf->lineDefined,tf->size,tf,tf->marked);
+ }
  V=tf->locvars;
  PrintCode(tf->code,tf->code+tf->size);
 }
