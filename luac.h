bc2e6be48538ea8c69756b4eb8304ae40a808705
Luiz Henrique de Figueiredo <lhf@tecgraf.puc-rio.br>
1997-09-11 12:54:17 -0300

    definitions for luac

--- luac.h
+++ luac.h
@@ -1,24 +1,16 @@
 /*
-** luac.h
-** definitions for luac compiler
-** $Id: luac.h,v 1.4 1996/03/06 15:59:30 lhf Exp lhf $
+** $Id: $
+** definitions for luac
+** Copyrightnotice
 */
 
-#include "inout.h"
-#include "mem.h"
-#include "opcode.h"
-#include "table.h"
-#include "undump.h"
+#include "auxlib.h"
+#include "lfunc.h"
+#include "lglobal.h"
+#include "lobject.h"
+#include "lopcodes.h"
+#include "lstring.h"
+#include "lundump.h"
 
-#define VarStr(i)	(lua_table[i].varname->str)
-#define VarLoc(i)	(lua_table[i].varname->varindex)
-#define StrStr(i)	(lua_constant[i]->str)
-#define StrLoc(i)	(lua_constant[i]->constindex)
-
-extern Word lua_ntable;
-extern Word lua_nconstant;
-extern int lua_debug;
-
-void DumpHeader(FILE* D);
-void DumpFunction(TFunc* tf, FILE* D);
-void PrintFunction(TFunc* tf);
+#define VarStr(i)	(luaG_global[i].varname->str)
+#define tfvalue(o)	((o)->value.tf)
