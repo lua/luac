913a08eef3d325ac459b1e1ac1f3518b868fce59
Luiz Henrique de Figueiredo <lhf@tecgraf.puc-rio.br>
1998-03-05 12:45:08 -0300

    now uses INFO removed FindFunction -- PrintFunctions now scans code

--- print.c
+++ print.c
@@ -1,5 +1,5 @@
 /*
-** $Id: print.c,v 1.6 1998/01/19 16:10:52 lhf Exp lhf $
+** $Id: print.c,v 1.7 1998/02/06 20:05:39 lhf Exp lhf $
 ** print bytecodes
 ** See Copyright Notice in lua.h
 */
@@ -8,27 +8,16 @@
 #include <stdlib.h>
 #include <string.h>
 #include "luac.h"
-#include "print.h"
-
-#ifdef DEBUG
-#define CHECKOP(op,p,code,tf,s) \
-if (op>=NOPCODES)			/* cannot happen */\
-	luaL_verror("internal error in %s: bad opcode %d at %d in tf=%p\n",\
-		s,op,(int)(p-code),tf)
-#else
-#define CHECKOP(op,p,code,tf,s)
-#endif
 
 int CodeSize(TProtoFunc* tf)		/* also used in dump.c */
 {
  Byte* code=tf->code;
- Byte* p=code+2;			/* skip headers bytes */
+ Byte* p=code;
  while (1)
  {
-  int op=*p;
-  CHECKOP(op,p,code,tf,"CodeSize");
-  p+=Opcode[op].size;
-  if (op==ENDCODE) break;
+  Opcode OP;
+  p+=INFO(tf,p,&OP);
+  if (OP.op==ENDCODE) break;
  }
  return p-code;
 }
@@ -37,7 +26,7 @@ int CodeSize(TProtoFunc* tf)		/* also used in dump.c */
 void PrintConstant1(TProtoFunc* tf, int i)
 {
  TObject* o=tf->consts+i;
- printf("%6d ",i,o->value.i);
+ printf("%6d ",i);
  switch (ttype(o))
  {
   case LUA_T_NUMBER:
@@ -57,8 +46,8 @@ void PrintConstant1(TProtoFunc* tf, int i)
 
 static void PrintConstants(TProtoFunc* tf)
 {
+ int i,n=tf->nconsts;
  int i;
- int n=tf->nconsts;
  printf("constants (%d):\n",n);
  for (i=0; i<n; i++) PrintConstant1(tf,i);
 }
@@ -77,9 +66,15 @@ static void PrintConstant(TProtoFunc* tf, int i)
   TObject* o=tf->consts+i;
   switch (ttype(o))
   {
-   case LUA_T_NUMBER:	printf("%g",(double)nvalue(o)); break;	/* LUA_NUMBER */
-   case LUA_T_STRING:	printf("\"%s\"",svalue(o)); break;
-   case LUA_T_PROTO:	printf("function at %p",tfvalue(o)); break;
+   case LUA_T_NUMBER:
+	printf("%g",(double)nvalue(o));		/* LUA_NUMBER */
+	break;
+   case LUA_T_STRING:
+	printf("\"%s\"",svalue(o));
+	break;
+   case LUA_T_PROTO:
+	printf("function at %p",tfvalue(o));
+	break;
    default:				/* cannot happen */
 #ifdef DEBUG
 	luaL_verror("internal error in PrintConstant: "
@@ -97,34 +92,23 @@ static void PrintCode(TProtoFunc* tf)
  Byte* code=tf->code;
  Byte* p=code;
  int line=0;
- printf("%6d  %02X%*s%-13s\t%d\n",(int)(p-code),*p,8,"","STACK",*p); p++;
- printf("%6d  %02X%*s",(int)(p-code),*p,8,"");
- if (*p>=ZEROVARARG)
-  printf("%-13s\t%d\n","VARARGS",*p-ZEROVARARG);
- else
-  printf("%-13s\t%d\n","ARGS",*p);
- p++;
  while (1)
  {
-	int op=*p;
-	int n,i;
-	CHECKOP(op,p,code,tf,"PrintCode");
-	n=Opcode[op].size;
+	Opcode OP;
+	int n=INFO(tf,p,&OP);
+	int op=OP.op;
+	int i=OP.arg;
 	printf("%6d  ",(int)(p-code));
 	{
 	 Byte* q=p;
 	 int i=n;
 	 while (i--) printf("%02X",*q++);
 	}
-	printf("%*s%-13s",2*(5-n),"",Opcode[op].name);
+	printf("%*s%-13s",2*(5-n),"",OP.name);
 
-	if (op==CALLFUNC || op==SETLIST) i=p[1];
-	else if (n==1) i=Opcode[op].arg;
-	else if (n==2) i=p[1];
-	else i=p[1]+(p[2]<<8);
-	if (n!=1) printf("\t%d",i); else if (i>=0) printf("\t");
+	if (n!=1 || op<0) printf("\t%d",i); else if (i>=0) printf("\t");
 
-	switch (Opcode[op].class)
+	switch (OP.class)
 	{
 
 	case ENDCODE:
@@ -152,11 +136,8 @@ static void PrintCode(TProtoFunc* tf)
 		break;
 
 	case SETLIST:
-		if (n>=3) printf(" %d",p[n-1]);
-		break;
-
 	case CALLFUNC:
-		if (n==3) printf(" %d",p[2]);
+		if (n>=3) printf(" %d",OP.arg2);
 		break;
 
 	case SETLINE:
@@ -221,65 +202,47 @@ static void PrintLocals(TProtoFunc* tf)
  printf("\n");
 }
 
-static Byte* FindFunction(TProtoFunc* tf, TProtoFunc* Main)
-{
- Byte* code=Main->code;
- Byte* p=code+2;			/* skip headers bytes */
- while (1)
- {
-  int op=*p;
-  int n,i;
-  CHECKOP(op,p,code,tf,"FindFunction");
-  if (op==ENDCODE) break;
-  n=Opcode[op].size;
-  if (n==1) i=Opcode[op].arg; else if (n==2) i=p[1]; else i=p[1]+(p[2]<<8);
-  op=Opcode[op].class;
-  if (op==PUSHCONSTANT)
-  {
-   TObject* o=Main->consts+i;
-   if (ttype(o)==LUA_T_PROTO && tfvalue(o)==tf) return p;
-  }
-  p+=n;
- }
-#ifdef DEBUG
- luaL_verror("internal error in FindFunction: function %p not found\n",tf);
-#endif
- return NULL;				/* avoid warnings */
-}
-
-static void PrintHeader(TProtoFunc* tf, TProtoFunc* Main)
+static void PrintHeader(TProtoFunc* tf, TProtoFunc* Main, int at)
 {
  int size=CodeSize(tf);
  if (IsMain(tf))
   printf("\nmain of \"%s\" (%d bytes at %p)\n",tf->fileName->str,size,tf);
  else if (Main)
  {
-  Byte* p=FindFunction(tf,Main);
   printf("\nfunction defined at \"%s\":%d (%d bytes at %p); used at ",
 	tf->fileName->str,tf->lineDefined,size,tf);
   if (IsMain(Main))
    printf("main");
   else
    printf("%p",Main);
-  printf("+%d\n",(int)(p-Main->code));
+  printf("+%d\n",at);
  }
 }
 
-static void PrintFunction(TProtoFunc* tf, TProtoFunc* Main);
+static void PrintFunction(TProtoFunc* tf, TProtoFunc* Main, int at);
 
-static void PrintFunctions(TProtoFunc* tf)
+static void PrintFunctions(TProtoFunc* Main)
 {
- int i,n=tf->nconsts;
- for (i=0; i<n; i++)
+ Byte* code=Main->code;
+ Byte* p=code;
+ while (1)
  {
-  TObject* o=tf->consts+i;
-  if (ttype(o)==LUA_T_PROTO) PrintFunction(tfvalue(o),tf);
+  Opcode OP;
+  int n=INFO(Main,p,&OP);
+  if (OP.class==ENDCODE) break;
+  if (OP.class==PUSHCONSTANT)
+  {
+   int i=OP.arg;
+   TObject* o=Main->consts+i;
+   if (ttype(o)==LUA_T_PROTO) PrintFunction(tfvalue(o),Main,(int)(p-code));
+  }
+  p+=n;
  }
 }
 
-static void PrintFunction(TProtoFunc* tf, TProtoFunc* Main)
+static void PrintFunction(TProtoFunc* tf, TProtoFunc* Main, int at)
 {
- PrintHeader(tf,Main);
+ PrintHeader(tf,Main,at);
  PrintLocals(tf);
  PrintCode(tf);
 #ifdef DEBUG
@@ -290,5 +253,5 @@ static void PrintFunction(TProtoFunc* tf, TProtoFunc* Main)
 
 void PrintChunk(TProtoFunc* Main)
 {
- PrintFunction(Main,0);
+ PrintFunction(Main,0,0);
 }
