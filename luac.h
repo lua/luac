ee56ccf7fcdcb8b27dd69e0ec0b1295223ea24dd
Luiz Henrique de Figueiredo <lhf@tecgraf.puc-rio.br>
1998-07-11 21:17:37 -0300

    added declaration for CodeSize added fileName macro so that tf->fileName can be NULL

--- luac.h
+++ luac.h
@@ -1,5 +1,5 @@
 /*
-** $Id: luac.h,v 1.4 1998/03/05 15:45:08 lhf Exp lhf $
+** $Id: luac.h,v 1.5 1998/03/30 11:22:25 lhf Exp lhf $
 ** definitions for luac
 ** See Copyright Notice in lua.h
 */
@@ -22,8 +22,10 @@ typedef struct
 } Opcode;
 
 int OpcodeInfo(TProtoFunc* tf, Byte* p, Opcode* I, char* xFILE, int xLINE);
+int CodeSize(TProtoFunc* tf);
 
 #define INFO(tf,p,I)	OpcodeInfo(tf,p,I,__FILE__,__LINE__)
+#define fileName(tf)	( (tf->fileName)==NULL ? NULL : tf->fileName->str )
 
 #define NOP	255
 #define STACK	-1
