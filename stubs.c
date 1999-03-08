93596c3759114438867ff4d291eccef9d0941630
Luiz Henrique de Figueiredo <lhf@tecgraf.puc-rio.br>
1999-03-08 08:08:43 -0300

    added several new stubs :-(

--- stubs.c
+++ stubs.c
@@ -1,5 +1,5 @@
 /*
-** $Id: stubs.c,v 1.8 1998/07/12 00:17:37 lhf Exp lhf $
+** $Id: stubs.c,v 1.9 1999/02/03 00:38:28 lhf Exp lhf $
 ** avoid runtime modules in luac
 ** See Copyright Notice in lua.h
 */
@@ -42,6 +42,21 @@ int luaL_findstring (char* name, char* list[])
  return -1;
 }
 
+/* copied from lauxlib.c */
+void luaL_chunkid (char *out, char *source, int len) {
+  len -= 13;  /* 13 = strlen("string ''...\0") */
+  if (*source == '@')
+    sprintf(out, "file `%.*s'", len, source+1);
+  else if (*source == '(')
+    strcpy(out, "(C code)");
+  else {
+    char *b = strchr(source , '\n');  /* stop string at first new line */
+    int lim = (b && (b-source)<len) ? b-source : len;
+    sprintf(out, "string `%.*s'", lim, source);
+    strcpy(out+lim+(13-5), "...'");  /* 5 = strlen("...'\0") */
+  }
+}
+
 /* avoid runtime modules in lstate.c */
 
 #include "lbuiltin.h"
@@ -54,9 +69,47 @@ void luaB_predefine(void){}
 void luaC_hashcallIM(Hash *l){}
 void luaC_strcallIM(TaggedString *l){}
 void luaD_gcIM(TObject *o){}
-void luaD_init(void){}
 void luaH_free(Hash *frees){}
 void luaT_init(void){}
+
+/* copied from ldo.c */
+
+#include "lmem.h"
+
+#ifndef STACK_LIMIT
+#define STACK_LIMIT     6000
+#endif
+
+#define STACK_UNIT	128
+
+void luaD_init (void) {
+  L->stack.stack = luaM_newvector(STACK_UNIT, TObject);
+  L->stack.top = L->stack.stack;
+  L->stack.last = L->stack.stack+(STACK_UNIT-1);
+}
+
+
+void luaD_checkstack (int n)
+{
+  struct Stack *S = &L->stack;
+  if (S->last-S->top <= n) {
+    StkId top = S->top-S->stack;
+    int stacksize = (S->last-S->stack)+1+STACK_UNIT+n;
+    S->stack = luaM_reallocvector(S->stack, stacksize, TObject);
+    S->last = S->stack+(stacksize-1);
+    S->top = S->stack + top;
+    if (stacksize >= STACK_LIMIT) {  /* stack overflow? */
+        lua_error("stack size overflow");
+    }
+  }
+}
+
+/* copied from lapi.c */
+int lua_setdebug (int debug) {
+  int old = L->debug;
+  L->debug = debug;
+  return old;
+}
 
 /*
 * the code below avoids the lexer and the parser (llex lparser).
@@ -69,10 +122,12 @@ void luaT_init(void){}
 #include "llex.h"
 #include "lparser.h"
 
-int lua_debug=0;
-
 void luaX_init(void){}
-void luaY_init(void){}
-void luaY_parser(void) { lua_error("parser not loaded"); }
+
+TProtoFunc* luaY_parser(ZIO *z) {
+ lua_error("parser not loaded");
+ return NULL;
+}
 
 #endif
+
