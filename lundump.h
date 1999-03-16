3db50e283266c0774f227a67905ccd9aa34b9547
Luiz Henrique de Figueiredo <lhf@tecgraf.puc-rio.br>
1999-03-16 15:15:53 -0300

    better instructions added luaU_testnumber

--- lundump.h
+++ lundump.h
@@ -1,5 +1,5 @@
 /*
-** $Id: lundump.h,v 1.9 1998/07/12 00:17:37 lhf Exp lhf $
+** $Id: lundump.h,v 1.10 1999/03/08 11:08:43 lhf Exp lhf $
 ** load pre-compiled Lua chunks
 ** See Copyright Notice in lua.h
 */
@@ -10,13 +10,45 @@
 #include "lobject.h"
 #include "lzio.h"
 
+/* -- start of user configuration ------------------------------------------ */
+
+/* LUA_NUMBER
+* choose below the number representation in precompiled chunks by choosing an
+*  an adequate definition for ID_NUMBER.
+* if you change LUA_NUM_TYPE, you must set ID_NUMBER accordingly.
+* the default is ID_REAL8 because the default for LUA_NUM_TYPE is double.
+* if you want to use this default, do nothing.
+* if your machine does not use the IEEE 754 floating-point standard, use
+*  ID_NATIVE, but precompiled chunks may not be portable to all architectures.
+*
+* for types other than the ones listed below, you'll have to write your own
+*  dump and undump routines, specially if sizeof(long)!=4.
+*/
+
+/* choose one definition for ID_NUMBER and move it to after #endif */
+#if 0
+#define	ID_NUMBER	ID_INT4		/* 4-byte integers */
+#define	ID_NUMBER	ID_REAL4	/* 4-byte reals */
+#define	ID_NUMBER	ID_REAL8	/* 8-byte reals */
+#define	ID_NUMBER	ID_NATIVE	/* whatever your machine uses */
+#endif
+
+/* format for numbers in listings and error messages */
+#ifndef NUMBER_FMT
+#define NUMBER_FMT	"%.16g"		/* LUA_NUMBER */
+#endif
+
+/* -- end of user configuration -- DO NOT CHANGE ANYTHING BELOW THIS LINE -- */
+
+
 TProtoFunc* luaU_undump1(ZIO* Z);	/* load one chunk */
+void luaU_testnumber(void);		/* test number representation */
 
 /* definitions for headers of binary files */
 #define	VERSION		0x32		/* last format change was in 3.2 */
 #define	VERSION0	0x32		/* last major  change was in 3.2 */
-#define ID_CHUNK	27		/* binary files start with ESC */
-#define	SIGNATURE	"Lua"		/* followed by this signature */
+#define ID_CHUNK	27		/* binary files start with ESC... */
+#define	SIGNATURE	"Lua"		/* ...followed by this signature */
 
 /* number representation */
 #define ID_INT4		'l'		/* 4-byte integers */
@@ -24,42 +56,14 @@ TProtoFunc* luaU_undump1(ZIO* Z);	/* load one chunk */
 #define ID_REAL8	'd'		/* 8-byte reals */
 #define ID_NATIVE	'?'		/* whatever your machine uses */
 
-/* how to format numbers in listings and error messages */
-#ifndef NUMBER_FMT
-#define NUMBER_FMT	"%.16g"		/* LUA_NUMBER */
-#endif
-
-/*
-* we use a multiple of PI for testing number representation.
-* multiplying by 1E8 gives non-trivial integer values.
-*/
-#define	TEST_NUMBER	3.14159265358979323846E8
-
-/* LUA_NUMBER
-* choose below the number representation in precompiled chunks.
-* you have to choose an adequate definition for ID_NUMBER.
-* the default is ID_REAL8 because the default for LUA_NUM_TYPE is double.
-* if your machine does not use IEEE 754, use ID_NATIVE.
-* a future version may include support portable conversion to and from IEEE 754.
-*
-* if you change LUA_NUM_TYPE, make sure you set ID_NUMBER accordingly,
-* specially if sizeof(long)!=4.
-* for types other than the ones listed below, you'll have to write your own
-* dump and undump routines.
-*/
-
+/* the default is ID_REAL8 because the default for LUA_NUM_TYPE is double */
 #ifndef ID_NUMBER
-#define	ID_NUMBER	ID_REAL8
+#define	ID_NUMBER	ID_REAL8	/* 8-byte reals */
 #endif
 
-#if 0
-#define	ID_NUMBER	ID_INT4
-#define	ID_NUMBER	ID_REAL4
-#define	ID_NUMBER	ID_REAL8
-#define	ID_NUMBER	ID_NATIVE
-#endif
-
-#endif
+/* a multiple of PI for testing number representation */
+/* multiplying by 1E8 gives non-trivial integer values */
+#define	TEST_NUMBER	3.14159265358979323846E8
 
 #if   ID_NUMBER==ID_REAL4
 	#define	DumpNumber	DumpFloat
@@ -79,4 +83,6 @@ TProtoFunc* luaU_undump1(ZIO* Z);	/* load one chunk */
 	#define SIZEOF_NUMBER	sizeof(real)
 #else
 	#error	bad ID_NUMBER
+#endif
+
 #endif
