912bab19d4e4fae1577875849bf4c359ff3e2a7d
Luiz Henrique de Figueiredo <lhf@tecgraf.puc-rio.br>
1999-12-02 16:51:09 -0200

    now reentrant

--- lundump.c
+++ lundump.c
@@ -1,11 +1,14 @@
 /*
-** $Id: lundump.c,v 1.22 1999/09/09 13:24:52 lhf Exp lhf $
+** $Id: lundump.c,v 1.23 1999/10/07 12:13:13 lhf Exp lhf $
 ** load bytecodes from files
 ** See Copyright Notice in lua.h
 */
 
 #include <stdio.h>
 #include <string.h>
+
+#define LUA_REENTRANT
+
 #include "lauxlib.h"
 #include "lfunc.h"
 #include "lmem.h"
@@ -13,183 +16,183 @@
 #include "lstring.h"
 #include "lundump.h"
 
-#define	LoadBlock(b,size,Z)	ezread(Z,b,size)
+#define	LoadBlock(L,b,size,Z)	ezread(L,Z,b,size)
 
-static void unexpectedEOZ (ZIO* Z)
+static void unexpectedEOZ (lua_State* L, ZIO* Z)
 {
- luaL_verror("unexpected end of file in %.255s",zname(Z));
+ luaL_verror(L,"unexpected end of file in %.255s",zname(Z));
 }
 
-static int ezgetc (ZIO* Z)
+static int ezgetc (lua_State* L, ZIO* Z)
 {
  int c=zgetc(Z);
- if (c==EOZ) unexpectedEOZ(Z);
+ if (c==EOZ) unexpectedEOZ(L,Z);
  return c;
 }
 
-static void ezread (ZIO* Z, void* b, int n)
+static void ezread (lua_State* L, ZIO* Z, void* b, int n)
 {
  int r=zread(Z,b,n);
- if (r!=0) unexpectedEOZ(Z);
+ if (r!=0) unexpectedEOZ(L,Z);
 }
 
-static unsigned int LoadWord (ZIO* Z)
+static unsigned int LoadWord (lua_State* L, ZIO* Z)
 {
- unsigned int hi=ezgetc(Z);
- unsigned int lo=ezgetc(Z);
+ unsigned int hi=ezgetc(L,Z);
+ unsigned int lo=ezgetc(L,Z);
  return (hi<<8)|lo;
 }
 
-static unsigned long LoadLong (ZIO* Z)
+static unsigned long LoadLong (lua_State* L, ZIO* Z)
 {
- unsigned long hi=LoadWord(Z);
- unsigned long lo=LoadWord(Z);
+ unsigned long hi=LoadWord(L,Z);
+ unsigned long lo=LoadWord(L,Z);
  return (hi<<16)|lo;
 }
 
-static real LoadNumber (ZIO* Z, int native)
+static real LoadNumber (lua_State* L, ZIO* Z, int native)
 {
  if (native)
  {
   real x;
-  LoadBlock(&x,sizeof(x),Z);
+  LoadBlock(L,&x,sizeof(x),Z);
   return x;
  }
  else
  {
   char b[256];
-  int size=ezgetc(Z);
-  LoadBlock(b,size,Z);
+  int size=ezgetc(L,Z);
+  LoadBlock(L,b,size,Z);
   b[size]=0;
-  return luaU_str2d(b,zname(Z));
+  return luaU_str2d(L,b,zname(Z));
  }
 }
 
-static int LoadInt (ZIO* Z, const char* message)
+static int LoadInt (lua_State* L, ZIO* Z, const char* message)
 {
- unsigned long l=LoadLong(Z);
+ unsigned long l=LoadLong(L,Z);
  unsigned int i=l;
- if (i!=l) luaL_verror(message,l,zname(Z));
+ if (i!=l) luaL_verror(L,message,l,zname(Z));
  return i;
 }
 
 #define PAD	5			/* two word operands plus opcode */
 
-static Byte* LoadCode (ZIO* Z)
+static Byte* LoadCode (lua_State* L, ZIO* Z)
 {
- int size=LoadInt(Z,"code too long (%lu bytes) in %.255s");
- Byte* b=luaM_malloc(size+PAD);
- LoadBlock(b,size,Z);
- if (b[size-1]!=ENDCODE) luaL_verror("bad code in %.255s",zname(Z));
+ int size=LoadInt(L,Z,"code too long (%lu bytes) in %.255s");
+ Byte* b=luaM_malloc(L,size+PAD);
+ LoadBlock(L,b,size,Z);
+ if (b[size-1]!=ENDCODE) luaL_verror(L,"bad code in %.255s",zname(Z));
  memset(b+size,ENDCODE,PAD);		/* pad code for safety */
  return b;
 }
 
-static TaggedString* LoadTString (ZIO* Z)
+static TaggedString* LoadTString (lua_State* L, ZIO* Z)
 {
- long size=LoadLong(Z);
+ long size=LoadLong(L,Z);
  if (size==0)
   return NULL;
  else
  {
-  char* s=luaL_openspace(size);
-  LoadBlock(s,size,Z);
-  return luaS_newlstr(s,size-1);
+  char* s=luaL_openspace(L,size);
+  LoadBlock(L,s,size,Z);
+  return luaS_newlstr(L,s,size-1);
  }
 }
 
-static void LoadLocals (TProtoFunc* tf, ZIO* Z)
+static void LoadLocals (lua_State* L, TProtoFunc* tf, ZIO* Z)
 {
- int i,n=LoadInt(Z,"too many locals (%lu) in %.255s");
+ int i,n=LoadInt(L,Z,"too many locals (%lu) in %.255s");
  if (n==0) return;
- tf->locvars=luaM_newvector(n+1,LocVar);
+ tf->locvars=luaM_newvector(L,n+1,LocVar);
  for (i=0; i<n; i++)
  {
-  tf->locvars[i].line=LoadInt(Z,"too many lines (%lu) in %.255s");
-  tf->locvars[i].varname=LoadTString(Z);
+  tf->locvars[i].line=LoadInt(L,Z,"too many lines (%lu) in %.255s");
+  tf->locvars[i].varname=LoadTString(L,Z);
  }
  tf->locvars[i].line=-1;		/* flag end of vector */
  tf->locvars[i].varname=NULL;
 }
 
-static TProtoFunc* LoadFunction (ZIO* Z, int native);
+static TProtoFunc* LoadFunction (lua_State* L, ZIO* Z, int native);
 
-static void LoadConstants (TProtoFunc* tf, ZIO* Z, int native)
+static void LoadConstants (lua_State* L, TProtoFunc* tf, ZIO* Z, int native)
 {
- int i,n=LoadInt(Z,"too many constants (%lu) in %.255s");
+ int i,n=LoadInt(L,Z,"too many constants (%lu) in %.255s");
  tf->nconsts=n;
  if (n==0) return;
- tf->consts=luaM_newvector(n,TObject);
+ tf->consts=luaM_newvector(L,n,TObject);
  for (i=0; i<n; i++)
  {
   TObject* o=tf->consts+i;
-  ttype(o)=-ezgetc(Z);			/* ttype(o) is negative - ORDER LUA_T */
+  ttype(o)=-ezgetc(L,Z);		/* ttype(o) is negative - ORDER LUA_T */
   switch (ttype(o))
   {
    case LUA_T_NUMBER:
-	nvalue(o)=LoadNumber(Z,native);
+	nvalue(o)=LoadNumber(L,Z,native);
 	break;
    case LUA_T_STRING:
-	tsvalue(o)=LoadTString(Z);
+	tsvalue(o)=LoadTString(L,Z);
 	break;
    case LUA_T_PROTO:
-	tfvalue(o)=LoadFunction(Z,native);
+	tfvalue(o)=LoadFunction(L,Z,native);
 	break;
    case LUA_T_NIL:
 	break;
    default:				/* cannot happen */
-	luaU_badconstant("load",i,o,tf);
+	luaU_badconstant(L,"load",i,o,tf);
 	break;
   }
  }
 }
 
-static TProtoFunc* LoadFunction (ZIO* Z, int native)
+static TProtoFunc* LoadFunction (lua_State* L, ZIO* Z, int native)
 {
- TProtoFunc* tf=luaF_newproto();
- tf->lineDefined=LoadInt(Z,"lineDefined too large (%lu) in %.255s");
- tf->source=LoadTString(Z);
- if (tf->source==NULL) tf->source=luaS_new(zname(Z));
- tf->code=LoadCode(Z);
- LoadLocals(tf,Z);
- LoadConstants(tf,Z,native);
+ TProtoFunc* tf=luaF_newproto(L);
+ tf->lineDefined=LoadInt(L,Z,"lineDefined too large (%lu) in %.255s");
+ tf->source=LoadTString(L,Z);
+ if (tf->source==NULL) tf->source=luaS_new(L,zname(Z));
+ tf->code=LoadCode(L,Z);
+ LoadLocals(L,tf,Z);
+ LoadConstants(L,tf,Z,native);
  return tf;
 }
 
-static void LoadSignature (ZIO* Z)
+static void LoadSignature (lua_State* L, ZIO* Z)
 {
  const char* s=SIGNATURE;
- while (*s!=0 && ezgetc(Z)==*s)
+ while (*s!=0 && ezgetc(L,Z)==*s)
   ++s;
- if (*s!=0) luaL_verror("bad signature in %.255s",zname(Z));
+ if (*s!=0) luaL_verror(L,"bad signature in %.255s",zname(Z));
 }
 
-static int LoadHeader (ZIO* Z)
+static int LoadHeader (lua_State* L, ZIO* Z)
 {
  int version,sizeofR,native;
- LoadSignature(Z);
- version=ezgetc(Z);
+ LoadSignature(L,Z);
+ version=ezgetc(L,Z);
  if (version>VERSION)
-  luaL_verror(
+  luaL_verror(L,
 	"%.255s too new: version=0x%02x; expected at most 0x%02x",
 	zname(Z),version,VERSION);
  if (version<VERSION0)			/* check last major change */
-  luaL_verror(
+  luaL_verror(L,
 	"%.255s too old: version=0x%02x; expected at least 0x%02x",
 	zname(Z),version,VERSION0);
- sizeofR=ezgetc(Z);
+ sizeofR=ezgetc(L,Z);
  native=(sizeofR!=0);
  if (native)				/* test number representation */
  {
   if (sizeofR!=sizeof(real))
-   luaL_verror("unknown number size in %.255s: read %d; expected %d",
+   luaL_verror(L,"unknown number size in %.255s: read %d; expected %d",
 	 zname(Z),sizeofR,(int)sizeof(real));
   else
   {
    real tf=TEST_NUMBER;
-   real f=LoadNumber(Z,native);
+   real f=LoadNumber(L,Z,native);
    if ((long)f!=(long)tf)
-    luaL_verror("unknown number format in %.255s: "
+    luaL_verror(L,"unknown number format in %.255s: "
 	  "read " NUMBER_FMT "; expected " NUMBER_FMT,
 	  zname(Z),f,tf);
   }
@@ -197,42 +200,42 @@ static int LoadHeader (ZIO* Z)
  return native;
 }
 
-static TProtoFunc* LoadChunk (ZIO* Z)
+static TProtoFunc* LoadChunk (lua_State* L, ZIO* Z)
 {
- return LoadFunction(Z,LoadHeader(Z));
+ return LoadFunction(L,Z,LoadHeader(L,Z));
 }
 
 /*
 ** load one chunk from a file or buffer
 ** return main if ok and NULL at EOF
 */
-TProtoFunc* luaU_undump1 (ZIO* Z)
+TProtoFunc* luaU_undump1 (lua_State* L, ZIO* Z)
 {
  int c=zgetc(Z);
  if (c==ID_CHUNK)
-  return LoadChunk(Z);
+  return LoadChunk(L,Z);
  else if (c!=EOZ)
-  luaL_verror("%.255s is not a Lua binary file",zname(Z));
+  luaL_verror(L,"%.255s is not a Lua binary file",zname(Z));
  return NULL;
 }
 
 /*
 * handle constants that cannot happen
 */
-void luaU_badconstant (const char* s, int i, const TObject* o, const TProtoFunc* tf)
+void luaU_badconstant (lua_State* L, const char* s, int i, const TObject* o, const TProtoFunc* tf)
 {
  int t=ttype(o);
  const char* name= (t>0 || t<LUA_T_LINE) ? "?" : luaO_typenames[-t];
- luaL_verror("cannot %s constant #%d: type=%d [%s]" IN,s,i,t,name,INLOC);
+ luaL_verror(L,"cannot %.255s constant #%d: type=%d [%s]" IN,s,i,t,name,INLOC);
 }
 
 /*
 * convert number from text
 */
-double luaU_str2d (const char* b, const char* where)
+double luaU_str2d (lua_State* L, const char* b, const char* where)
 {
  double x;
  if (!luaO_str2d(b,&x))
-  luaL_verror("cannot convert number '%.255s' in %.255s",b,where);
+  luaL_verror(L,"cannot convert number '%.255s' in %.255s",b,where);
  return x;
 }
--- lundump.h
+++ lundump.h
@@ -1,5 +1,5 @@
 /*
-** $Id: lundump.h,v 1.15 1999/07/02 19:34:26 lhf Exp lhf $
+** $Id: lundump.h,v 1.16 1999/09/09 13:24:52 lhf Exp lhf $
 ** load pre-compiled Lua chunks
 ** See Copyright Notice in lua.h
 */
@@ -10,20 +10,24 @@
 #include "lobject.h"
 #include "lzio.h"
 
-TProtoFunc* luaU_undump1 (ZIO* Z);	/* load one chunk */
-void luaU_badconstant (const char* s, int i, const TObject* o, const TProtoFunc* tf);
-					/* handle cases that cannot happen */
-double luaU_str2d (const char* b, const char* where);
-					/* convert number from text */
+/* load one chunk */
+TProtoFunc* luaU_undump1 (lua_State* L, ZIO* Z);
+
+/* handle cases that cannot happen */
+void luaU_badconstant (lua_State* L, const char* s, int i,
+			const TObject* o, const TProtoFunc* tf);
+
+/* convert number from text */
+double luaU_str2d (lua_State* L, const char* b, const char* where);
 
 /* definitions for headers of binary files */
-#define	VERSION		0x32		/* last format change was in 3.2 */
-#define	VERSION0	0x32		/* last major  change was in 3.2 */
+#define	VERSION		0x33		/* last format change was in 3.3 */
+#define	VERSION0	0x33		/* last major  change was in 3.3 */
 #define ID_CHUNK	27		/* binary files start with ESC... */
 #define	SIGNATURE	"Lua"		/* ...followed by this signature */
 
 /* formats for error messages */
-#define SOURCE		"<%s:%d>"
+#define SOURCE		"<%.255s:%d>"
 #define IN		" in %p " SOURCE
 #define INLOC		tf,tf->source->str,tf->lineDefined
 
