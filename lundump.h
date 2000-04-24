0da4d04223a17ae4b8b6bb376076335161759d8a
Luiz Henrique de Figueiredo <lhf@tecgraf.puc-rio.br>
2000-04-24 14:32:29 -0300

    update version removed luaU_badconstant added TEST_CODE

--- lundump.h
+++ lundump.h
@@ -1,5 +1,5 @@
 /*
-** $Id: lundump.h,v 1.17 1999/12/02 18:51:09 lhf Exp lhf $
+** $Id: lundump.h,v 1.18 2000/01/28 17:51:09 lhf Exp lhf $
 ** load pre-compiled Lua chunks
 ** See Copyright Notice in lua.h
 */
@@ -11,18 +11,14 @@
 #include "lzio.h"
 
 /* load one chunk */
-TProtoFunc* luaU_undump1 (lua_State* L, ZIO* Z);
-
-/* handle cases that cannot happen */
-void luaU_badconstant (lua_State* L, const char* s, int i,
-			const TObject* o, const TProtoFunc* tf);
+Proto* luaU_undump1 (lua_State* L, ZIO* Z);
 
 /* convert number from text */
 double luaU_str2d (lua_State* L, const char* b, const char* where);
 
 /* definitions for headers of binary files */
-#define	VERSION		0x33		/* last format change was in 3.3 */
-#define	VERSION0	0x33		/* last major  change was in 3.3 */
+#define	VERSION		0x40		/* last format change was in 4.0 */
+#define	VERSION0	0x40		/* last major  change was in 4.0 */
 #define ID_CHUNK	27		/* binary files start with ESC... */
 #define	SIGNATURE	"Lua"		/* ...followed by this signature */
 
@@ -40,5 +36,8 @@ double luaU_str2d (lua_State* L, const char* b, const char* where);
 /* a multiple of PI for testing native format */
 /* multiplying by 1E8 gives non-trivial integer values */
 #define	TEST_NUMBER	3.14159265358979323846E8
+
+/* something for testing byte order in instructions */
+#define	TEST_CODE	0x01020304
 
 #endif
