37bcd5a75a5ef213bd210c9fed76de269110eab6
Luiz Henrique de Figueiredo <lhf@tecgraf.puc-rio.br>
2001-03-15 14:29:16 -0300

    loops until end of code, not until OP_END

--- print.c
+++ print.c
@@ -1,5 +1,5 @@
 /*
-** $Id: print.c,v 1.31 2000/10/31 16:57:23 lhf Exp $
+** $Id: print.c,v 1.32 2000/11/06 20:04:36 lhf Exp lhf $
 ** print bytecodes
 ** See Copyright Notice in lua.h
 */
@@ -14,17 +14,17 @@
 #define P_NONE
 #define P_AB	printf("%d %d",GETARG_A(i),GETARG_B(i))
 #define P_F	printf("%d %d\t; %p",GETARG_A(i),GETARG_B(i),tf->kproto[GETARG_A(i)])
-#define P_J	printf("%d\t; to %d",GETARG_S(i),GETARG_S(i)+at+1)
+#define P_J	printf("%d\t; to %d",GETARG_S(i),GETARG_S(i)+pc+2)
 #define P_Q	PrintString(tf,GETARG_U(i))
-#define P_K	printf("%d\t; %s",GETARG_U(i),tf->kstr[GETARG_U(i)]->str)
-#define P_L	PrintLocal(tf,GETARG_U(i),at-1)
+#define P_K	printf("%d\t; %s",GETARG_U(i),getstr(tf->kstr[GETARG_U(i)]))
+#define P_L	PrintLocal(tf,GETARG_U(i),pc)
 #define P_N	printf("%d\t; " NUMBER_FMT,GETARG_U(i),tf->knum[GETARG_U(i)])
 #define P_S	printf("%d",GETARG_S(i))
 #define P_U	printf("%u",GETARG_U(i))
 
 static void PrintString(const Proto* tf, int n)
 {
- const char* s=tf->kstr[n]->str;
+ const char* s=getstr(tf->kstr[n]);
  printf("%d\t; ",n);
  putchar('"');
  for (; *s; s++)
@@ -55,20 +55,22 @@ static void PrintLocal(const Proto* tf, int n, int pc)
 static void PrintCode(const Proto* tf)
 {
  const Instruction* code=tf->code;
- const Instruction* p=code;
- for (;;)
+ int pc,n=tf->sizecode;
+ for (pc=0; pc<n; pc++) 
  {
-  int at=p-code+1;
-  Instruction i=*p;
-  int line=luaG_getline(tf->lineinfo,at-1,1,NULL);
-  printf("%6d\t",at);
+  Instruction i=code[pc];
+  int line=luaG_getline(tf->lineinfo,pc,1,NULL);
+#if 1
+  printf("%0*lX",Sizeof(i)*2,i);
+#endif
+if(0)  printf("  %6d\t",pc+1);
+  printf("\t%d\t",pc+1);
   if (line>=0) printf("[%d]\t",line); else printf("[-]\t");
-  switch (GET_OPCODE(i)) {
+  switch (GET_OPCODE(i))
+  {
 #include "print.h"
   }
   printf("\n");
-  if (i==OP_END) break;
-  p++;
  }
 }
 
@@ -81,18 +83,18 @@ static void PrintHeader(const Proto* tf)
 {
  printf("\n%s " SOURCE_FMT " (%d instruction%s/%d bytes at %p)\n",
  	IsMain(tf)?"main":"function",SOURCE,
-	S(tf->ncode),tf->ncode*Sizeof(Instruction),tf);
+	S(tf->sizecode),tf->sizecode*Sizeof(Instruction),tf);
  printf("%d%s param%s, %d stack%s, ",
 	tf->numparams,tf->is_vararg?"+":"",SS(tf->numparams),S(tf->maxstacksize));
  printf("%d local%s, %d string%s, %d number%s, %d function%s, %d line%s\n",
-	S(tf->nlocvars),S(tf->nkstr),S(tf->nknum),S(tf->nkproto),S(tf->nlineinfo));
+	S(tf->sizelocvars),S(tf->sizekstr),S(tf->sizeknum),S(tf->sizekproto),S(tf->sizelineinfo));
 }
 
 #define PrintFunction luaU_printchunk
 
 void PrintFunction(const Proto* tf)
 {
- int i,n=tf->nkproto;
+ int i,n=tf->sizekproto;
  PrintHeader(tf);
  PrintCode(tf);
  for (i=0; i<n; i++) PrintFunction(tf->kproto[i]);
