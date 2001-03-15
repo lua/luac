e56d3f80f03f8424973dc941db9b44037c52a617
Luiz Henrique de Figueiredo <lhf@tecgraf.puc-rio.br>
2001-03-15 14:29:16 -0300

    new version use getstr

--- lundump.h
+++ lundump.h
@@ -1,5 +1,5 @@
 /*
-** $Id: lundump.h,v 1.20 2000/09/18 20:03:46 lhf Exp lhf $
+** $Id: lundump.h,v 1.21 2000/10/31 16:57:23 lhf Exp lhf $
 ** load pre-compiled Lua chunks
 ** See Copyright Notice in lua.h
 */
@@ -14,17 +14,17 @@
 Proto* luaU_undump (lua_State* L, ZIO* Z);
 
 /* find byte order */
-int luaU_endianess (void);
+int luaU_endianness (void);
 
 /* definitions for headers of binary files */
-#define	VERSION		0x40		/* last format change was in 4.0 */
-#define	VERSION0	0x40		/* last major  change was in 4.0 */
+#define	VERSION		0x41		/* last format change was in 4.1 */
+#define	VERSION0	0x41		/* last major  change was in 4.1 */
 #define ID_CHUNK	27		/* binary files start with ESC... */
 #define	SIGNATURE	"Lua"		/* ...followed by this signature */
 
 /* formats for error messages */
 #define SOURCE_FMT	"<%d:%.99s>"
-#define SOURCE		tf->lineDefined,tf->source->str
+#define SOURCE		tf->lineDefined,getstr(tf->source)
 #define IN_FMT		" in %p " SOURCE_FMT
 #define IN		tf,SOURCE
 
