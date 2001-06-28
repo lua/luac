cb22fce0a4174efa8a0029268ef4d6077253f037
Luiz Henrique de Figueiredo <lhf@tecgraf.puc-rio.br>
2001-06-28 10:55:17 -0300

    removed SOURCE_FMT and SOURCE and others

--- lundump.h
+++ lundump.h
@@ -1,5 +1,5 @@
 /*
-** $Id: lundump.h,v 1.21 2000/10/31 16:57:23 lhf Exp lhf $
+** $Id: lundump.h,v 1.22 2001/03/15 17:29:16 lhf Exp lhf $
 ** load pre-compiled Lua chunks
 ** See Copyright Notice in lua.h
 */
@@ -21,12 +21,6 @@ int luaU_endianness (void);
 #define	VERSION0	0x41		/* last major  change was in 4.1 */
 #define ID_CHUNK	27		/* binary files start with ESC... */
 #define	SIGNATURE	"Lua"		/* ...followed by this signature */
-
-/* formats for error messages */
-#define SOURCE_FMT	"<%d:%.99s>"
-#define SOURCE		tf->lineDefined,getstr(tf->source)
-#define IN_FMT		" in %p " SOURCE_FMT
-#define IN		tf,SOURCE
 
 /* a multiple of PI for testing native format */
 /* multiplying by 1E8 gives non-trivial integer values */
