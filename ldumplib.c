688c52287bdab0cd4bdc228c882ee029d648fd52
Luiz Henrique de Figueiredo <lhf@tecgraf.puc-rio.br>
2001-06-28 10:55:17 -0300

    only exports predump

--- ldumplib.c
+++ ldumplib.c
@@ -1,14 +1,17 @@
 /*
-** $Id$
+** $Id: ldumplib.c,v 1.1 2001/03/15 17:29:16 lhf Exp lhf $
 ** library access to precompiler
 ** See Copyright Notice in lua.h
 */
 
 #include <stdio.h>
 
+#define LUA_PRIVATE
+#include "lua.h"
+#include "lualib.h"
 #include "lauxlib.h"
-#include "lparser.h"
-#include "lundump.h"
+
+LUALIB_API int lua_dumplibopen (lua_State *L);
 
 static void myfputc(int c, luaL_Buffer* B) {
  char b[1];
@@ -25,46 +28,29 @@ static void myfwrite(const void* b, size_t size, size_t n, luaL_Buffer* B) {
 #define WRITE	myfwrite
 #include "dump.c"
 
-static int prestring(lua_State *L) {
- size_t l;
- const char* s=luaL_check_lstr(L,1,&l); 
- ZIO z;
- luaL_Buffer b;
- luaL_buffinit(L,&b);
- luaZ_mopen(&z,s,l,s);
- luaU_dumpchunk(luaY_parser(L,&z),&b);
- luaL_pushresult(&b);
- return 1;
-}
-
-static int prefile(lua_State *L) {
- const char* filename=luaL_check_string(L,1); 
- ZIO z;
- char source[512];
- FILE* f=fopen(filename,"r");
- luaL_Buffer b;
- if (f==NULL) {
-  lua_pushnil(L);
+static int predump(lua_State *L) {
+ if (!lua_isfunction(L,1) || lua_iscfunction(L,1)) {
+  luaL_argerror(L,1,"Lua function expected");
+  return 0;
+ }
+ else {
+  luaL_Buffer b;
+  const Closure* c=lua_topointer(L,1);
+  luaL_buffinit(L,&b);
+  luaU_dumpchunk(c->f.l,&b);
+  luaL_pushresult(&b);
   return 1;
  }
- sprintf(source,"@%.*s",(int)sizeof(source)-2,filename);
- luaL_buffinit(L,&b);
- luaZ_Fopen(&z,f,source);
- luaU_dumpchunk(luaY_parser(L,&z),&b);
- if (f!=stdin) fclose(f);
- luaL_pushresult(&b);
- return 1;
 }
 
 static const struct luaL_reg dumplib[] = {
-{"prestring", prestring},
-{"prefile", prefile},
+{"predump", predump},
 };
 
 /*
 ** Open dump library
 */
-int lua_dumplibopen (lua_State *L) {
+LUALIB_API int lua_dumplibopen (lua_State *L) {
  luaL_openl(L,dumplib);
  return 0;
 }
