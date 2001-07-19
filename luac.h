225e9dab1dc281284e6a768172bd4bf0b8471a12
Luiz Henrique de Figueiredo <lhf@tecgraf.puc-rio.br>
2001-07-19 11:34:06 -0300

    moved Sizeof to print.c removed luac_state

--- luac.h
+++ luac.h
@@ -1,5 +1,5 @@
 /*
-** $Id: luac.h,v 1.19 2001/03/15 17:29:16 lhf Exp lhf $
+** $Id: luac.h,v 1.20 2001/06/28 13:55:17 lhf Exp lhf $
 ** definitions for luac
 ** See Copyright Notice in lua.h
 */
@@ -9,15 +9,11 @@
 
 #include "lobject.h"
 
-extern lua_State *luac_state;		/* lazy! */
-
 /* from dump.c */
 void luaU_dumpchunk(const Proto* Main, FILE* D);
 
 /* from opt.c */
-void luaU_optchunk(Proto* Main);
+void luaU_optchunk(lua_State* L, Proto* f);
 
 /* from print.c */
 void luaU_printchunk(const Proto* Main);
-
-#define Sizeof(x)	((int)sizeof(x))
