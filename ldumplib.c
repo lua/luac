9db30aea2523dc9f8cb70945d36dd7a35d3e2fc3
Luiz Henrique de Figueiredo <lhf@tecgraf.puc-rio.br>
2002-06-06 10:22:56 -0300

    luaU_dump now receives writer function waiting to be incorporated into lbaselib.c

--- ldumplib.c
+++ ldumplib.c
@@ -1,60 +1,39 @@
 /*
-** $Id: ldumplib.c,v 1.5 2001/11/29 01:26:28 lhf Exp lhf $
+** $Id: ldumplib.c,v 1.6 2002/02/28 20:09:28 lhf Exp lhf $
 ** library access to precompiler
 ** See Copyright Notice in lua.h
 */
 
-#include <stdio.h>
+#include <stddef.h>
 
 #include "lua.h"
 #include "lualib.h"
 #include "lauxlib.h"
 
+#include "lobject.h"
+#include "lundump.h"
+
 LUALIB_API int lua_dumplibopen (lua_State *L);
 
-static void myfputc(int c, luaL_Buffer* B) {
- char b[1];
- b[0]=(char)c;
- luaL_addlstring(B,b,1);
+static size_t writer(const void* b, size_t size, size_t n, void* B) {
+ luaL_addlstring((luaL_Buffer*)B,b,size*n);
+ return n;
 }
-
-static void myfwrite(const void* b, size_t size, size_t n, luaL_Buffer* B) {
- luaL_addlstring(B,b,size*n);
-}
-
-#define WRITETO	luaL_Buffer*
-#define WRITE1	myfputc
-#define WRITE	myfwrite
-#include "dump.c"
 
 static int dump(lua_State *L) {
- if (!lua_isfunction(L,1)) {
-  luaL_typerror(L,1,"Lua function");
-  return 0;
- }
- else if (lua_iscfunction(L,1)) {
-  luaL_argerror(L,1,"cannot dump C functions");
-  return 0;
- }
- else {
-  luaL_Buffer b;
-  const Closure* c=lua_topointer(L,1);
-  if (c->l.nupvalues>0) luaL_argerror(L,1,"cannot dump closures");
-  luaL_buffinit(L,&b);
-  luaU_dumpchunk(c->l.p,&b);
-  luaL_pushresult(&b);
-  return 1;
- }
+ luaL_Buffer b;
+ const Closure* c=lua_topointer(L,1);
+ if (c->l.nupvalues>0) luaL_argerror(L,1,"cannot dump closures");
+ luaL_buffinit(L,&b);
+ luaU_dump(c->l.p,writer,&b);
+ luaL_pushresult(&b);
+ return 1;
 }
-
-static const struct luaL_reg dumplib[] = {
-{"dump", dump},
-};
 
 /*
 ** Open dump library
 */
 LUALIB_API int lua_dumplibopen (lua_State *L) {
- luaL_openl(L,dumplib);
+ lua_register(L,"dump",dump);
  return 0;
 }
